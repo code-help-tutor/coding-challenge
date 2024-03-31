3d29784e401be2feb01c9e9b40e01675e0c0b2b0
Code Help Tutor <1823890830@qq.com>
2024-03-31 03:16:10 +0000

    WeChat cstutorcs coding challenge

---
+++ packages/app/next-env.d.ts
@@ -0,0 +1,8 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
---
+++ packages/react/.storybook/storybook-theme.ts
@@ -0,0 +1,13 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import { create } from "@storybook/theming"
+// @ts-ignore
+import brandImage from "./storybook-logo.svg"
+
+export default create({
+  base: "light",
+  brandTitle: "Chakra UI",
+  brandUrl: "https://chakra-ui.com",
+  brandImage,
+})
